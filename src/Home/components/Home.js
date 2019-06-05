644c4d7c38c78a7ee0104aa2ef73527bd2596f37
Kalpana Nagan <nkalpana5771@gmail.com>
2019-06-04 21:42:43 -0400

    Style change

--- src/Home/components/Home.js
+++ src/Home/components/Home.js
@@ -18,7 +18,7 @@ class Home extends Component {
                     
                         <div className="container mt-2">
                             <p><i className="fa fa-briefcase fa-fw mr-3 lead text-info"></i>Full Stack Developer</p>
-                            <p><i className="fa fa-home fa-fw mr-3 lead text-info"></i>Bangalore, Karnataka</p>
+                            <p><i className="fa fa-home fa-fw mr-3 lead text-info"></i>Toronto, ON , Canada</p>
                             <p><i className="fa fa-envelope fa-fw mr-3 lead text-info"></i>nkalpana@gmail.com</p>
                             <p><i className="fa fa-phone fa-fw mr-3 lead text-info"></i>+1 6478632316</p>
                             <hr/>
@@ -47,6 +47,11 @@ class Home extends Component {
                             <p>HTML, CSS and Javascript</p>
                             <div className="progress mb-3" style={{height: '18px', borderRadius: '20px'}}>
                                 <div className="progress-bar bg-info" style={{width:'80%'}}>80%</div>
+                            </div>
+
+                            <p>Java</p>
+                            <div className="progress mb-3" style={{height: '18px', borderRadius: '20px'}}>
+                                <div className="progress-bar bg-info" style={{width:'80%'}}>75%</div>
                             </div>
 
                             <hr/>
@@ -78,21 +83,21 @@ class Home extends Component {
                         <div className="container">
                             <h5 className="header1"><b>Full Stack Developer / CGI</b></h5>
                             <h6 className="text-info"><i className="fa fa-calendar fa-fw mr-3"></i>Mar 2015 - <span className="badge bg-info text-light">Jun 2018</span></h6>
-                            <p>As a full stack developer, I work on React.js and Java.</p>
+                            <p>As a full stack developer, worked on various projects using Javascrip libraries and Java</p>
                             <hr/>
                         </div>
                         <div className="container mb-4">
                             <h5 className="header1"><b>Frontend web devloper / Robert Bosch</b></h5>
                             <h6 className="text-info"><i className="fa fa-calendar fa-fw mr-3"></i>Jul 2018 - Nov 2018</h6>
-                            <p>As a Frontend web developer, developed responsive web application.</p>
+                            <p>As a Frontend web developer, developed responsive web applications.</p>
                         </div>
                     </div>
 
                     <div className="container card mb-4 box">
-                        <h2 className="header1 p-3"><i className="fa fa-certificate fa-fw mr-3 text-info"></i>Education</h2>
+                        <h2 className="header1 p-3"><i className="fas fa-book-open fa-fw mr-3 text-info"></i>Education</h2>
                         <div className="container">
                             <h5 className="header1"><b>SSLC</b></h5>
-                            <h6 className="text-info"><i className="fa fa-calendar fa-fw mr-3"></i>2006 - 2007</h6>
+                            <h6 className="text-info"><i className="fas fa-calendar fa-fw mr-3"></i>2006 - 2007</h6>
                             <p>Shastry High School, Hunsur</p>
                             <hr/>
                         </div>
@@ -122,23 +127,23 @@ class Home extends Component {
             <p>Find me on social media.</p>
             <ul className="list-inline m-0">
                 <li className="list-inline-item">
-                    <a className="text-light" rel="noopener noreferrer" href="" target="_blank">
-                        <i className="fa fa-facebook-official lead"></i>
+                    <a className="text-light" rel="noopener noreferrer" href="https://www.facebook.com/kalpana.nagan" target="_blank">
+                    <i class="fab fa-facebook-square"></i>
                     </a>
                 </li>
                 <li className="list-inline-item">
                     <a className="text-light" rel="noopener noreferrer" href="https://www.instagram.com/Kalpana_Nagan/" target="_blank">
-                        <i className="fa fa-instagram lead"></i>
+                    <i className="fab fa-instagram"></i>
                     </a>
                 </li>
                 <li className="list-inline-item">
                     <a className="text-light" rel="noopener noreferrer" href="https://linkedin.com/in/kalpana-n-4b972577/" target="_blank">
-                        <i className="fa fa-linkedin lead"></i>
+                    <i className="fab fa-linkedin"></i>
                     </a>
                 </li>
                 <li className="list-inline-item">
-                    <a className="text-light" rel="noopener noreferrer" href="" target="_blank">
-                        <i className="fa fa-google-plus lead"></i>
+                    <a className="text-light" rel="noopener noreferrer" href="https://twitter.com/KalpanaNagan" target="_blank">
+                        <i className="fab fa-twitter-square"></i>
                     </a>
                 </li>
             </ul>
